9a1b05c98307ac16f7d659db5c887fa87f839f3b
Rusiton <galassosantiago05@gmail.com>
2025-08-31 03:27:24 -0300

    Created basic migrations and models

---
+++ app/Models/Board.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Board extends Model
+{
+    protected $fillable = [
+        'name',
+        'description',
+        'owner_id',
+    ];
+
+
+    
+    public function user(): BelongsTo
+    {
+        return $this->BelongsTo(User::class);
+    }
+
+
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+
+
+    public function columns(): HasMany
+    {
+        return $this->hasMany(Column::class);
+    }
+}
---
+++ app/Models/Task.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Task extends Model
+{
+    protected $fillable = [
+        'list_id',
+        'assigned_to',
+        'name',
+        'description',
+        'position',
+        'due_date',
+    ];
+
+
+
+    public function column(): BelongsTo
+    {
+        return $this->belongsTo(Column::class);
+    }
+
+
+
+    public function assigned_to(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
+    }
+}
